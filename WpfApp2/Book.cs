4a025cc43199d88c937f50fdf1b0628c60e604fc
damir-ilyasov <damir2006ilyasov@gmail.com>
2026-02-12 13:56:34 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Book.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishYear { get; set; }
+        public string ISBN { get; set; }
+        public string Genre { get; set; }
+        public int QuantilityInStock { get; set; }
+    }
+}
